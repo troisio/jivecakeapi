5ff7e2f1976fb0950bf0390ddead782cd4b95274
Luis Banegas Saybe <luis@Luiss-MacBook-Air.local>
2016-10-19 13:49:54 -0400

    feat(event): Public search has order and time of creation

--- src/main/java/com/jivecake/api/model/Organization.java
+++ src/main/java/com/jivecake/api/model/Organization.java
@@ -8,13 +8,15 @@ import org.mongodb.morphia.annotations.Field;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Index;
 import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.utils.IndexType;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.jivecake.api.serializer.ObjectIdSerializer;
 
 @Entity
 @Indexes({
-    @Index(fields = @Field("email"))
+    @Index(fields = @Field("email")),
+    @Index(fields = @Field(value = "name", type = IndexType.TEXT))
 })
 public class Organization {
     @Id
