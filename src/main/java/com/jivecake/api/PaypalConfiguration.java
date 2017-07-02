afd212140a0fe071a47bef634c400c7b840a8ca6
Luis E. Banegas Saybe <luis@trois.io>
2017-07-01 22:57:58 -0400

    fix(gzip): Gzip not on oauth resource

--- src/main/java/com/jivecake/api/PaypalConfiguration.java
+++ src/main/java/com/jivecake/api/PaypalConfiguration.java
@@ -1,7 +1,6 @@
 package com.jivecake.api;
 
 public class PaypalConfiguration {
-    public String accessToken;;
     public String clientId;
     public String clientSecret;
     public String mode;
